12071413ea4216cca22875e955944c94f15939ac
atsushi.koga <stbe51@gmail.com>
2019-02-24 23:26:07 +0900

    get movie list by api

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,28 +1,28 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.movie.Title }}</div>
     </div>
 </template>
 <script>
     import genres from '../util/genres';
 
     export default {
-        data() {
-            return {
-                movies: [
-                    {title: 'Kill Bill', genre: genres.CRIME},
-                    {title: 'Home Alone', genre: genres.COMEDY},
-                    {title: 'Austin Powers', genre: genres.COMEDY}
-                ]
-            };
-        },
-        props: ['genre', 'time'],
+    // data() {
+    //     return {
+    //         movies: [
+    //             {title: 'Kill Bill', genre: genres.CRIME},
+    //             {title: 'Home Alone', genre: genres.COMEDY},
+    //             {title: 'Austin Powers', genre: genres.COMEDY}
+    //         ]
+    //     };
+    // },
+        props: ['genre', 'time', 'movies'],
         methods: {
             moviePassesGenreFilter(movie){
                 if(this.genre.length === 0){
                     return true;
                 }else{
-                    return this.genre.find(genre => movie.genre === genre)
+                    return this.genre.find(genre => movie.movie.Genre === genre)
                 }
             }
         },
