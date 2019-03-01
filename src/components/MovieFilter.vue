18be67ac116dfb7b4e2b861774352fc350596927
atsushi.koga <stbe51@gmail.com>
2019-03-01 17:01:13 +0900

    filter movie by time

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,8 +1,13 @@
 <template>
     <div id="movie-filter">
         <h2>Filter results</h2>
+        <h3>By time of day</h3>
         <div class="filter-group">
-            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+            <check-filter v-for="time in times" category="time" v-bind:title="time" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+        <h3>By genre</h3>
+        <div class="filter-group">
+            <check-filter v-for="genre in genres" category="genre" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
         </div>
     </div>
 </template>
@@ -10,11 +15,13 @@
 <script>
     import genres from '../util/genres';
     import CheckFilter from './CheckFilter.vue';
+    import times from '../util/times';
 
     export default{
         data() {
             return {
-                genres
+                genres,
+                times
             }
         },
         methods: {
